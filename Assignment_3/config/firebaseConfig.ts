d971b8a9e71bc516a3327734151638bc27a6d5db
Klima2 <klima2@rrc.ca>
2026-02-17 22:15:54 -0600

    fix Stop tracking firebase service account key.

--- Assignment_3/config/firebaseConfig.ts
+++ Assignment_3/config/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
 // update with your won firebase private key .json file path
-import * as serviceAccount from "./path-to-your-firebase-service-account.json";
+import * as serviceAccount from "../my-project-assignment-3-21243-firebase-adminsdk-fbsvc-c56e290851.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
